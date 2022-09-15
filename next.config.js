ed7136ae5d58957faa8ccd9cbd32f6c106182f19
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-09-14 23:17:05 -0300

    config

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  images: {
+    domains: ['files.stripe.com'],
+  },
+
+  experimental: {
+    newNextLinkBehavior: true,
+    images: {
+      allowFutureImage: true,
+    },
+  },
+};
+
+module.exports = nextConfig;
